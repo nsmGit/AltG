344bc6a07c498e38420df89a15736eaffeccdb8f
soreiyu <soreiyu52@gmail.com>
2018-07-15 15:31:18 +0900

    login Display

--- AltG/settings.py
+++ AltG/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'workStatus.apps.WorkstatusConfig',
-    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
